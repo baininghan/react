edb532e4247e151aafb0ba07836ca0066f73d940
renfufei <renfufei@qq.com>
2015-01-01 17:42:21 +0800

    JS格式化

--- js/examples/hello.js
+++ js/examples/hello.js
@@ -1,13 +1,29 @@
-var HELLO_COMPONENT = ("\nvar HelloMessage = React.createClass({\n  render: function() {\n    return <div>Hello {this.props.name}</div>;\n  }\n});\n\nReact.render(<HelloMessage name=\"John\" />, mountNode);\n"
-
-
-
-
-
-
+// 这只是字符串
+var HELLO_COMPONENT = 
+(
+	"\n"
+	+"var HelloMessage = React.createClass("
+		+"{"
+		+"\n"
+		+"  render: function() {"
+		+"\n"
+		+"/"+"/"+"你可以修改此处的代码,右边将动态显示结果"
+		+"\n"
+		+"    return <div>Hello {this.props.name}</div>;"
+		+"\n"
+		+"  }"
+		+"\n"
+		+"}"
+	+");"
+	
+	+"\n\n"
+	+"React.render(<HelloMessage name=\"John\" />, mountNode);"
+	+"\n"
 
 );
 
+// 这个组件不错哦
+// 如果你看到这个源码,其实就是这里起作用啦.
 React.render(
   React.createElement(ReactPlayground, {codeText: HELLO_COMPONENT}),
   document.getElementById('helloExample')
--- js/examples/timer.js
+++ js/examples/timer.js
@@ -1,27 +1,50 @@
-var TIMER_COMPONENT = ("\nvar Timer = React.createClass({\n  getInitialState: function() {\n    return {secondsElapsed: 0};\n  },\n  tick: function() {\n    this.setState({secondsElapsed: this.state.secondsElapsed + 1});\n  },\n  componentDidMount: function() {\n    this.interval = setInterval(this.tick, 1000);\n  },\n  componentWillUnmount: function() {\n    clearInterval(this.interval);\n  },\n  render: function() {\n    return (\n      <div>Seconds Elapsed: {this.state.secondsElapsed}</div>\n    );\n  }\n});\n\nReact.render(<Timer />, mountNode);\n"
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// 这只是字符串
+var TIMER_COMPONENT = 
+(
+	"\n"
+	+"var Timer = React.createClass("
+		+"{"
+		+"\n"
+		+"  getInitialState: function() {"
+		+"\n"
+		+"    return {secondsElapsed: 0};"
+		+"\n"
+		+"  },"
+		+"\n"
+		+"  tick: function() {"
+		+"\n"
+		+"    this.setState({secondsElapsed: this.state.secondsElapsed + 1});"
+		+"\n"
+		+"  },"
+		+"\n"
+		+"  componentDidMount: function() {"
+		+"\n    this.interval = setInterval(this.tick, 1000);"
+		+"\n"
+		+"  },"
+		+"\n"
+		+"  componentWillUnmount: function() {"
+		+"\n    clearInterval(this.interval);"
+		+"\n"
+		+"  },"
+		+"\n"
+		+"  render: function() {"
+		+"\n"
+		+"    return ("
+		+"\n      <div>Seconds Elapsed: {this.state.secondsElapsed}</div>"
+		+"\n"
+		+"    );"
+		+"\n"
+		+"  }"
+		+"\n"
+		+"}"
+		+");"
+	+"\n"
+	+"\n"
+	+"React.render(<Timer />, mountNode);"
+	+"\n"
 
 );
-
+// ReactPlayground 组件
 React.render(
   React.createElement(ReactPlayground, {codeText: TIMER_COMPONENT}),
   document.getElementById('timerExample')
